43f8efcf4388598ea7cf68b1d6e1320af010b238
dimikmps <dimitris.kmps@gmail.com>
2024-05-22 12:41:58 +0300

    Create first version of landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Box, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
    return (
       <>
-         <div>
-            <h1>rESPonder-v.2</h1>
-            <p>Version 2 for the rESPonder Client</p>
-         </div>
+         <Box display='flex' flexDirection='column' justifyContent='space-between' height='33vh'>
+            <Box display='flex' justifyContent='center'>
+               <Typography variant='h2' display='flex' flexDirection='column' justifyContent='flex-end'>
+                  rESPonder
+               </Typography>
+               <Typography
+                  display='flex'
+                  flexDirection='column'
+                  justifyContent='flex-end'
+                  height='100%'
+                  fontSize='10px'
+               >
+                  v.2
+               </Typography>
+            </Box>
+            <h2>An IoT application for disaster rescue scenarios</h2>
+         </Box>
       </>
    );
 }
