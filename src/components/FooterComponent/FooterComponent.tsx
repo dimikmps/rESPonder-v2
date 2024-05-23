a9a45192cfde43e454e720b0ecf08ff20544524e
dimikmps <dimitris.kmps@gmail.com>
2024-05-23 12:49:21 +0300

    Add prettier config

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -4,15 +4,25 @@ import { Box, Link, Typography } from '@mui/material';
  * Footer Component
  **/
 const FooterComponent = () => {
-   return (
-      <Box display='flex' flexDirection='column' justifyContent='center' width='100%' margin='0 auto'>
-         <Typography fontSize='10px' textAlign='right' color='white'>
-            Copyright{' '}
-            <Link color='inherit' underline='none' href='https://www.linkedin.com/in/dkampas/'>
-               Dimitris Kampas
-            </Link>
-         </Typography>
-      </Box>
-   );
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            width="100%"
+            margin="0 auto"
+        >
+            <Typography fontSize="10px" textAlign="right" color="white">
+                Copyright{' '}
+                <Link
+                    color="inherit"
+                    underline="none"
+                    href="https://www.linkedin.com/in/dkampas/"
+                >
+                    Dimitris Kampas
+                </Link>
+            </Typography>
+        </Box>
+    );
 };
 export default FooterComponent;
