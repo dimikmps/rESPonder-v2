87e583450eaf66c37712fdae3f32163d58b9b5dd
Dimitris K <dimitris.kmps@gmail.com>
2024-05-23 15:36:40 +0300

    Style/t004 add formatting linting config (#3)
    - Add linting & formatting configs
    - Add pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,48 @@ import HeaderComponent from './components/HeaderComponent/HeaderComponent';
 import './App.css';
 
 function App() {
-   return (
-      <Box display='flex' flexDirection='column' justifyContent='space-between' height='100vh'>
-         <HeaderComponent />
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      height="100vh"
+    >
+      <HeaderComponent />
 
-         <Box display='flex' flexDirection='column' justifyContent='center' gap='66px' height='100%'>
-            <Box display='flex' justifyContent='center'>
-               <Typography variant='h2' display='flex' flexDirection='column' justifyContent='flex-end'>
-                  rESPonder
-               </Typography>
-               <Typography
-                  display='flex'
-                  flexDirection='column'
-                  justifyContent='flex-end'
-                  height='100%'
-                  fontSize='10px'
-               >
-                  v.2
-               </Typography>
-            </Box>
-            <Typography display='flex' justifyContent='center' fontSize='20px'>
-               An IoT application for disaster rescue scenarios
-            </Typography>
-         </Box>
-         <FooterComponent />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        gap="66px"
+        height="100%"
+      >
+        <Box display="flex" justifyContent="center">
+          <Typography
+            variant="h2"
+            display="flex"
+            flexDirection="column"
+            justifyContent="flex-end"
+          >
+            rESPonder
+          </Typography>
+          <Typography
+            display="flex"
+            flexDirection="column"
+            justifyContent="flex-end"
+            height="100%"
+            fontSize="10px"
+          >
+            v.2
+          </Typography>
+        </Box>
+        <Typography display="flex" justifyContent="center" fontSize="20px">
+          An IoT application for disaster rescue scenarios
+        </Typography>
       </Box>
-   );
+      <FooterComponent />
+    </Box>
+  );
 }
 
 export default App;
--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -2,40 +2,49 @@ import { Box, Link, styled } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 // Mock Header Menu list. TODO: Replace with actual menu items
-const mockMenuList: string[] = ['Menu Item 1', 'Menu Item 2', 'Menu Item 3', 'Menu Item 4'];
+const mockMenuList: string[] = [
+  'Menu Item 1',
+  'Menu Item 2',
+  'Menu Item 3',
+  'Menu Item 4',
+];
 
 const HeaderMenuLink = styled(Link)({
-   display: 'flex',
-   textDecoration: 'none',
-   color: 'inherit',
-   fontSize: '18px',
+  display: 'flex',
+  textDecoration: 'none',
+  color: 'inherit',
+  fontSize: '18px',
 
-   // TODO: Add cool hover effect
-   '&:hover': {
-      color: '#ff9900',
-      transition: 'all .5s ease',
-   },
+  // TODO: Add cool hover effect
+  '&:hover': {
+    color: '#ff9900',
+    transition: 'all .5s ease',
+  },
 });
 
 /*
- * Header Component
+ * Main Header Component
  **/
 const HeaderComponent = () => {
-   return (
-      <Box display='flex' justifyContent='space-between' width='100%'>
-         <Box display='flex' justifyContent='center' gap='100px' width='100%'>
-            {mockMenuList.map((menuItem: string) => {
-               return <HeaderMenuLink href='#'>{menuItem}</HeaderMenuLink>;
-            })}
-         </Box>
-         <Box display='flex' minWidth='max-content'>
-            <HeaderMenuLink display='flex' gap='8px' alignItems='center'>
-               <AccountCircleIcon />
-               Sign in
+  return (
+    <Box display="flex" justifyContent="space-between" width="100%">
+      <Box display="flex" justifyContent="center" gap="100px" width="100%">
+        {mockMenuList.map((menuItem: string, index: number) => {
+          return (
+            <HeaderMenuLink href="#" key={index}>
+              {menuItem}
             </HeaderMenuLink>
-         </Box>
+          );
+        })}
       </Box>
-   );
+      <Box display="flex" minWidth="max-content">
+        <HeaderMenuLink display="flex" gap="8px" alignItems="center">
+          <AccountCircleIcon />
+          Sign in
+        </HeaderMenuLink>
+      </Box>
+    </Box>
+  );
 };
 
 export default HeaderComponent;
