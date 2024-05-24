ca9cd7efbb69fa50cf55ad0d109a86968a38a3be
Dimitris K <dimitris.kmps@gmail.com>
2024-05-24 15:02:23 +0300

    Merge first/plain app version and readme file.

---
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -0,0 +1,28 @@
+import { Box, Link, Typography } from '@mui/material';
+
+/*
+ * Main Footer Component
+ **/
+const FooterComponent = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      width="100%"
+      margin="0 auto"
+    >
+      <Typography fontSize="10px" textAlign="right" color="white">
+        Copyright{' '}
+        <Link
+          color="inherit"
+          underline="none"
+          href="https://www.linkedin.com/in/dkampas/"
+        >
+          Dimitris Kampas
+        </Link>
+      </Typography>
+    </Box>
+  );
+};
+export default FooterComponent;
