33604a96a060779923188c58018fd1cefb105940
dimikmps <dimitris.kmps@gmail.com>
2024-06-20 14:26:34 +0300

    Refactor map, add sensor locations/markers, refine design

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,5 +1,12 @@
 import { useTheme } from '@mui/material/styles';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Circle,
+  //   Polyline,
+} from 'react-leaflet';
 import { Box, Typography } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
@@ -19,6 +26,13 @@ const MapPage = (): JSX.Element => {
     ref.current && setContentHeight(ref.current.offsetHeight + 'px');
   }, [ref.current]);
 
+  // Calculate distance to draw a radius-based line from the center of the circle to its periphery
+  //   const earthRadius = 6378.137; //radius of the earth in kilometer
+  //   const pi = Math.PI;
+  //   const degreeConversion = 1 / (((2 * pi) / 360) * earthRadius) / 1000; //1 meter in degree
+  //   // TODO: Add this dynamically
+  //   const newLatitude = 23.6628 + 7800 * degreeConversion;
+
   return (
     <Box
       display='flex'
@@ -35,17 +49,43 @@ const MapPage = (): JSX.Element => {
       </Typography>
 
       <MapContainer
-        center={[51.505, -0.09]}
+        center={[39.9417, 23.6628]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         style={{ height: `calc(100vh - ${contentHeight})`, width: '100wh' }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup></Popup>
+        <Circle center={[39.9417, 23.6628]} fillColor='grey' radius={6000} />
+        68
+        <Circle center={[39.9417, 23.6628]} fillColor='blue' radius={25} />
+        {/* <Polyline
+          positions={[
+            [39.9417, 23.6628],
+            [39.9417, newLatitude],
+          ]}
+        /> */}
+        <Marker position={[39.9417, 23.6628]}>
+          <Popup>
+            <div>Centralised Node - Town Hall</div>
+          </Popup>
+        </Marker>
+        <Marker position={[39.9594, 23.6837]}>
+          <Popup>
+            <div>Sensor 1 - [39.9594, 23.6837]</div>
+          </Popup>
+        </Marker>
+        <Marker position={[39.93045, 23.72228]}>
+          <Popup>
+            <div>Sensor 2 - [39.93045, 23.72228]</div>
+          </Popup>
+        </Marker>
+        <Marker position={[39.9569, 23.6242]}>
+          <Popup>
+            <div>Sensor 3 - [39.9569, 23.6242]</div>
+          </Popup>
         </Marker>
       </MapContainer>
     </Box>
