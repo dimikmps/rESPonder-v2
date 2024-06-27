6a3c537eac6f1426c6d7a51bf5ff8be828f77717
Dimitris K <dimitris.kmps@gmail.com>
2024-06-27 16:30:52 +0300

    Dev (#39)
    Align latest dev into main.

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -28,8 +28,8 @@ const AboutPage: React.FC = () => {
     <PageTemplateComponent pageTitle='About'>
       <Box
         width='100%'
-        height='50%'
-        p={theme.spacing(8)}
+        height='100%'
+        px={theme.spacing(8)}
         display='flex'
         flexDirection='column'
         justifyContent='space-around'
@@ -43,13 +43,31 @@ const AboutPage: React.FC = () => {
           <ListItem sx={{ display: 'list-item' }}>
             <Typography sx={{ fontWeight: '600' }}>Map View</Typography>
             <Typography variant='subtitle2'>
-              Displays the current locations of sensors deployed in the
-              Paliouri, Chalkidiki (GR) area. You can view all deployed sensors
-              or select a specific one from the main dropdown menu. The central
-              node, located on a government building, is always visible on the
-              map and relays sensor data from nearby areas to the cloud-based
-              (AWS) back-end service. The displayed range corresponds to a 6km
-              radius, the optimal LoRa communication range.
+              This section displays the current locations of sensors deployed in
+              the Paliouri, Chalkidiki (GR) area, which was selected for the
+              experimental deployment in the original implementation. Users can
+              view all deployed sensors or select a specific one from the main
+              dropdown menu.
+              <Typography variant='subtitle2'>
+                A central node, located on a government building, is always
+                visible on the map. This node relays sensor data from nearby
+                areas to the cloud-based (AWS) back-end service. The blue circle
+                on the map represents a 6km radius, indicating the optimal LoRa
+                communication range.
+              </Typography>
+              <Typography variant='subtitle2'>
+                Clicking on each sensor icon provides additional information,
+                such as its coordinates and a link to the specific sensor&#39;s
+                data streaming page.
+              </Typography>
+              <Typography variant='subtitle2'>
+                The main dropdown menu allows users to choose between displaying
+                each of the three deployed sensors individually or all sensors
+                simultaneously.
+              </Typography>
+              <Typography variant='subtitle2'>
+                Sensor data is retrieved and updated every second.
+              </Typography>
             </Typography>
             <Typography variant='subtitle2'>
               Clicking on each sensor in the vicinity provides additional
@@ -62,9 +80,11 @@ const AboutPage: React.FC = () => {
               Latest Sensor Readings
             </Typography>
             <Typography variant='subtitle2'>
-              Provides real-time streaming data from sensors, including the
-              number of registered and unregistered devices near the selected
-              sensor.
+              This section provides real-time streaming data from the sensors,
+              including the number of registered and unregistered devices near
+              the selected sensor. Users can choose to display data for a
+              specific sensor via the dropdown menu. The data is updated every
+              second.
             </Typography>
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>
@@ -77,11 +97,15 @@ const AboutPage: React.FC = () => {
               </Typography>
             </Box>
             <Typography variant='subtitle2'>
-              Displays RSSI-based proximity data calculated by AWS Lambda every
-              five seconds. This includes information about pre-registered
-              devices near each sensor and offers sorting capabilities through
-              an underlying table to help identify the closest registered
-              devices (and potential users) in the area.
+              This section displays RSSI-based proximity data, calculated by AWS
+              Lambda every five seconds. It includes information about
+              pre-registered devices near each sensor and offers sorting
+              capabilities through a table to help identify the closest
+              registered devices (and potential users) in the area. The
+              displayed data is updated every five seconds and corresponds to
+              the sensor currently selected via the main dropdown menu. Any
+              device present in the selected sensor&#39;s proximity will appear
+              in the table.
             </Typography>
           </ListItem>
         </List>
