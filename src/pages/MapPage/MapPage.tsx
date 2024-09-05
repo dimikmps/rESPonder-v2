cfe65862bce1a43690cf085cebe91a3af16c20fc
dimikmps <dimitris.kmps@gmail.com>
2024-09-05 11:47:30 +0300

    Fix marker for production builds

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -5,7 +5,17 @@ import { Box, Typography } from '@mui/material';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { SensorContext } from '../../contexts/SensorContext';
 import { SensorLocationDataType } from '../../interfaces/SensorLocationData.interface';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import L from 'leaflet';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 /**
  * Map Page View
